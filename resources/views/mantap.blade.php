3456b397e4be5a03a2a06d8522a586a79a681785
Asulll-edan <muhsulrafi32@gmail.com>
2026-04-08 15:00:15 +0700

    sia panop bau

---
+++ resources/views/mantap.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.dashboard')
+
+@section('kata-kata')
+<h3>name: {{ nama }}</h3>
+<h3>{{ kata }}</h3>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::redirect('/home', '/mantap');
+
+Route::view('/mantap','sia mantap', [
+    'nama' => 'asul',
+    'kata'=> 'ini teman saya dibawah namanya?'
+
+]);
